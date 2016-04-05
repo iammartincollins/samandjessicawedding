42bdfcbf8ee493d769a8820cf10feca564db1ba9
Martin Collins <martin.collins.uk@gmail.com>
2016-04-05 21:12:16 +0100

    added venue page with google map

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,17 @@
 var gulp = require('gulp');
 var less = require('gulp-less');
+var concat = require('gulp-concat');
 
 gulp.task('less', function () {
     return gulp.src('./app/assets/less/site.less')
         .pipe(less())
         .pipe(gulp.dest('./app/public/css'));
 });
+
+gulp.task('scripts', function() {
+    return gulp.src('./app/assets/**/*.js')
+        .pipe(concat('site.js'))
+        .pipe(gulp.dest('./app/public/js/'));
+});
+
+gulp.task('build', ['less', 'scripts']);
